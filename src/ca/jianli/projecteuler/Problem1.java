d17daa862abcf4c5c7772ec6b6d26c7383df73f5
Jian Li <me@jianli.ca>
2014-03-23 22:51:47 -0700

    Added new solution to problem 1

--- src/ca/jianli/projecteuler/Problem1.java
+++ src/ca/jianli/projecteuler/Problem1.java
@@ -1,4 +1,5 @@
 package ca.jianli.projecteuler;
+
 /**
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
  * get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -20,7 +21,19 @@ class Problem1 {
 		return sum;
 	}
 
+	static int solution2(int N) {
+		int target = N - 1;
+		return sumOfMultiple(3, target) + sumOfMultiple(5, target)
+				- sumOfMultiple(15, target);
+	}
+
+	private static int sumOfMultiple(int i, int N) {
+		int n = N / i;
+		return n * (i * (n + 1)) / 2;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(solution(1000));
+		System.out.println(solution2(1000));
 	}
 }
